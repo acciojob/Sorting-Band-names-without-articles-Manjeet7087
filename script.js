b9423b1d47e66e4c75cff4c425b6b4edeefd653c
Manjeet7087 <131547570+Manjeet7087@users.noreply.github.com>
2023-06-11 19:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
+let bandNames = ['The Rolling Stones', 'Red Hot Chili Peppers', 'Led Zeppelin', 'The Beatles', 'Pink Floyd'];
+
+function sortBandNames(names) {
+  const articles = ['a', 'an', 'the'];
+  
+  // Custom comparison function to sort the band names
+  const compare = (a, b) => {
+    const aWords = a.toLowerCase().split(' ');
+    const bWords = b.toLowerCase().split(' ');
+
+    // Remove articles from band names for comparison
+    const cleanA = aWords.filter(word => !articles.includes(word));
+    const cleanB = bWords.filter(word => !articles.includes(word));
+
+    return cleanA.join(' ').localeCompare(cleanB.join(' '));
+  };
+
+  // Sort the band names using the custom comparison function
+  names.sort(compare);
+}
+
+sortBandNames(bandNames);
+
+const ulElement = document.getElementById('band');
+bandNames.forEach(name => {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});
 
